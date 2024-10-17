da622ad91c648999c6809fb7ab0abe09a94bd692
GessyAvila <gessyavila91@gmail.com>
2024-10-17 14:58:41 -0600

    Initial Commit

---
+++ PikaToolAPP/PikaToolAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  PikaToolAPPApp.swift
+//  PikaToolAPP
+//
+//  Created by Gessy Avila on 17/10/24.
+//
+
+import SwiftUI
+
+@main
+struct PikaToolAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PikaToolAPPTests/PikaToolAPPTests.swift
@@ -0,0 +1,17 @@
+//
+//  PikaToolAPPTests.swift
+//  PikaToolAPPTests
+//
+//  Created by Gessy Avila on 17/10/24.
+//
+
+import Testing
+@testable import PikaToolAPP
+
+struct PikaToolAPPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
